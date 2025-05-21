c14f47b94aaccb414b16e7d48b8cf9e8c1c31b94
simchaya <simchacb@gmail.com>
2025-05-21 14:12:39 -0700

    added auth.js under middleware folder

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+// Middleware to check if user is authenticated
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  }
+
+  module.exports = { isLoggedIn }; //to avoid overwriting the exports of app.js
